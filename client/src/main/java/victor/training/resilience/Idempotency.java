28a1cbaf9f19ea98307184ab998588e05645b82c
Victor Rentea <victorrentea@gmail.com>
2024-03-12 00:03:40 +0200

    yolo

--- client/src/main/java/victor/training/resilience/Idempotency.java
+++ client/src/main/java/victor/training/resilience/Idempotency.java
@@ -2,20 +2,21 @@ package victor.training.resilience;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class IdempotencyKey {
+public class Idempotency {
   @PostMapping("order")
-  public void placeOrder() {
+  public void placeOrder(@RequestBody String order) {
 
   }
 
   private final Set<String> lastSeenIdempotencyKeys = Collections.synchronizedSet(new HashSet<>());
   @PutMapping("order/{idempotencyKey}")
-  public void placeOrder(String idempotencyKey) {
+  public void placeOrder(String idempotencyKey, @RequestBody String order) {
     if (!lastSeenIdempotencyKeys.add(idempotencyKey)) {
       throw new RuntimeException("Duplocated order request with idempotencyKey " + idempotencyKey);
     }
